5daac0c9342fb334c16a9a5c3a21b2630431eff0
Jonatan <jontalas@gmail.com>
2021-12-29 04:09:16 +0100

    Entrega

---
+++ Laboratorio6_2.js
@@ -0,0 +1,43 @@
+// Constantes.
+const plainAlphabet = "abcdefghijklmnopqrstuvwxyz:()!¡,'";
+const encryptedAlphabet = "qw,ert(yuio'pa:sdfg!hjklz¡xcv)bnm";
+
+// Funciones para el apartado de la encriptación ------------------------------------
+var processText = (isEncrypt) => {
+    if (isEncrypt) {
+        crypt(plainAlphabet, encryptedAlphabet, document.getElementById("decryptedText").value, document.getElementById("encryptedText"));
+    }
+    else {
+        crypt(encryptedAlphabet, plainAlphabet, document.getElementById("encryptedText").value, document.getElementById("decryptedText"));
+    }
+}
+var crypt = (fromAlphabet, toAlphabet, fromText, toText) => {
+    var result = "";
+    for (const key in fromText) {
+        if (fromAlphabet.indexOf(fromText[key]) == -1) {
+            result += fromText[key];
+        }
+        else {
+            result += toAlphabet[fromAlphabet.indexOf(fromText[key])];
+        }
+    };
+    toText.value = result;
+}
+
+// Funciones para el apartado de la generación aleatoria ------------------------------------
+var randomPick = (n, min, max) => {
+    var result = [];
+    const range = max - min + 1;
+    while (!(result.length >= n || result.length >= range)) {
+        var newValue;
+        do {
+            newValue = Math.floor(Math.random() * range) + min;
+        } while (result.indexOf(newValue) != -1);
+        result.push(newValue);
+    }
+    return result;
+}
+// TEST generación aleatoria
+console.log("Primitiva: " + randomPick(6, 1, 49)); // Apuesta automática de la primitiva
+console.log("Bolas de billar: " + randomPick(15, 1, 15)); // Escoge combinación de bolas de billar
+console.log("Tirada de dado: " + randomPick(1, 1, 6)); // Tirada aleatoria de un dado
